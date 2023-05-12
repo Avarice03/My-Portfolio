4be802462da3fb9d97ff497318d6af3a29e5e27f
Niel <dmendegorin@gmail.com>
2023-05-12 18:14:30 +0800

    testing layout

---
+++ mendegorin-portfolio/assets/scripts/app.js
@@ -0,0 +1,9 @@
+new fullpage('#fullpage', {
+	//options here
+	autoScrolling:true,
+	scrollHorizontally: true,
+    navigation: true,
+    slidesNavigation: true,
+    slidesNavPosition: "bottom",
+    responsiveWidth: 1100,
+});
