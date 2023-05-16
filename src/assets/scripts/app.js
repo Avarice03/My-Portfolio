fff97bc8b209b5bb8a87c1d4b84010123b5c2b4f
Niel <dmendegorin@gmail.com>
2023-05-16 21:36:51 +0800

    feat: added contact page

--- src/assets/scripts/app.js
+++ src/assets/scripts/app.js
@@ -1,13 +1,7 @@
 new fullpage("#fullpage", {
   //options here
 
-  anchors: [
-    "home",
-    "skills",
-    "projects",
-    "contact",
-    "footer",
-  ],
+  anchors: ["home", "skills", "projects", "contact", "footer"],
   autoScrolling: true,
   scrollHorizontally: true,
   navigation: true,
@@ -18,8 +12,14 @@ new fullpage("#fullpage", {
 });
 
 const menuButton = document.querySelector(".menuButton");
+const submitBtn = document.querySelector("#submitBtn");
 
 menuButton.addEventListener("click", Menu);
+submitBtn.addEventListener("click", sendEmail);
+
+const scriptURL =
+  "https://script.google.com/macros/s/AKfycby9rHXNNTohmk-EDm_b8vX3kLRJ20iEQhaCCu_msa2-ubNl8s2utJjm8spGite1f2Pk/exec";
+const form = document.getElementById("submit-to-google-sheet");
 
 function Menu(e) {
   let list = document.querySelector("ul");
@@ -33,3 +33,37 @@ function Menu(e) {
     list.classList.remove("opacity-100");
   }
 }
+
+function validateEmail(email) {
+  if (/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g.test(email)) {
+    alert("Email sent successfully");
+    return true;
+  } else {
+    alert("You have entered an invalid email address!");
+    return false;
+  }
+}
+
+function sendEmail() {}
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const nameInput = document.getElementById("nameInput");
+  const emailInput = document.getElementById("emailInput");
+  const messageInput = document.getElementById("messageInput");
+
+  const name = nameInput.value;
+  const email = emailInput.value;
+
+  if (validateEmail(email)) {
+    fetch(scriptURL, { method: "POST", body: new FormData(form) })
+      .then((response) => {
+        console.log("Success!", response);
+        nameInput.value = "";
+        emailInput.value = "";
+        messageInput.value = "";
+      })
+      .catch((error) => console.error("Error!", error.message));
+    name.textContent = "";
+  }
+});
